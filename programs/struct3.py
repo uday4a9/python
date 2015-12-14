148dc4ab92d472fe97f51c02be827683574eef31
uday4a9 <uday4a9@gmail.com>
2015-12-14 18:20:11 +0530

    After few chages: 1. Just added the code for to make the object is an iterable. 2. Over ridden the length method to count the number of childs.

--- programs/struct3.py
+++ programs/struct3.py
@@ -1,14 +1,31 @@
 #! /usr/bin/env /usr/bin/python3
 
 
-class Node():
+class Node(object):
     def __init__(self, value):
-        print ("Node created successfully..")
+        #print ("Node created successfully..")
         self._value = value
         self._child = []
+        self._len = 0
 
     def __repr__(self):
         return "Node : {!r}".format(self._value)
+
+    def add_child(self, child):
+        self._len += 1
+        self._child.append(child)
+
+    def __iter__(self):
+        '''
+         To create iterator object on top of the child list
+	'''
+        return iter(self._child)
+
+    def __len__(self):
+        '''
+         Which returns always how amny no.of child nodes it contains.
+	'''
+        return self._len 
 
 
 
@@ -16,6 +33,16 @@ if __name__ == '__main__':
     root = Node(0)
     left = Node(1)
     right = Node(2)
-    print (root)
-    print (left)
-    print (right)
+    #print (root)
+    #print (left)
+    #print (right)
+
+    root.add_child(left)
+    root.add_child(right)
+
+    print(root, "Contains :", len(root), "childs")
+    # Here iterator protocol invokes and convert it to be handled
+    # by for loop.
+    for node in root:
+        print(node, "contains :", len(node), "childs")
+
