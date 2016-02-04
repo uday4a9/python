0bac025529d8874545e20b156e9d02fea09f2e39
uday4a9 <uday4a9@gmail.com>
2016-02-04 15:48:59 +0530

    After changing the code to handle __enter__ and __exit__ methods properly.

--- programs/context3.py
+++ programs/context3.py
@@ -5,13 +5,13 @@ import sys
 class File(object):
     def __init__(self, fil):
         self.__fil = fil
-        print("Context object created")
+#        print("Context object created")
 
     def __enter__(self):
-        print("executed the enter part")
+        print('=' * 10 + "\nexecuted the enter part")
 
-    def __exit__(self):
-        print("executed the exit part")
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("executed the exit part\n" + '=' * 10)
 
 if __name__ == '__main__':
     assert (len(sys.argv) == 2)
