48714118a43d26f0866e807d9666b9dbe151dda8
uday4a9 <uday4a9@gmail.com>
2016-07-26 21:52:12 +0530

    changed

--- programs/negbin.py
+++ programs/negbin.py
@@ -2,9 +2,9 @@
 def getbin(i):
         dig = []
     while i:
-                i, rem = divmod(i, -2)
+        i, rem = divmod(i, -2)
         if rem<0:
-                        i, rem = i+1, rem+2
+            i, rem = i+1, rem+2
         dig.insert(0, str(rem))
     dig = [int(i) for i in dig]
     dig.reverse()
@@ -15,7 +15,7 @@ def solution(A):
     entropy = 1
     val = A[0] * 1
     for i in range(1, len(A)):
-                entropy *= -2
+        entropy *= -2
         val += (A[i] * entropy)
     val = ~val + 1
     
