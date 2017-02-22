278fb12db1d746d31aaf3664ec9098055d02aac0
uday4a9 <uday4a9@gmail.com>
2017-02-22 07:43:48 +0530

    Added the code to traverse even under lists also

--- programs/json_files/json_getter.py
+++ programs/json_files/json_getter.py
@@ -7,27 +7,30 @@ from pprint import pprint
 def get_targetnode_in_json(d, node, path):
     if not isinstance(d, abc.Mapping):
         return
+
     if node in d:
         path.append(node)
         print('.'.join(path), d[node], sep=" => ")
         path.pop()
+
     for key in d:
         path.append(key)
         if isinstance(d[key], abc.Mapping):
             get_targetnode_in_json(d[key], node, path)
+        if isinstance(d[key], abc.MutableSequence):
+            for index in range(len(d[key])):
+                path.append(str(index))
+                get_targetnode_in_json(d[key][index], node, path)
+                path.pop()
         path.pop()
-
-
-
 
 def main(target_node=None):
     d = json.load(open('sim2.json'))
-    pprint(d)
-    print('=*=' * 25)
+    #pprint(d)
+    #print('=*=' * 25)
     for key in d:
         get_targetnode_in_json(d[key], target_node, [key])
 
 if __name__ == '__main__':
     #main(target_node="name")
-    main(target_node="badges")
-
+    main(target_node="fav")
